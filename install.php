6cfd53b311a6c656e495cd36df91a89cdb3a5bbd
Antonio <64821480+MINTILER-DEV@users.noreply.github.com>
2025-05-12 14:19:28 -0500

    Create install.php

---
+++ install.php
@@ -0,0 +1,32 @@
+<?php
+// install.php - WebComposer installer
+
+// Create directory structure
+$structure = [
+    'vendor',
+    'src'
+];
+
+foreach ($structure as $dir) {
+    if (!file_exists($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
+$files = [
+    'WebComposer.php',
+    'PackageResolver.php',
+    'AutoloadGenerator.php',
+    'Semver.php',
+    'HttpClient.php'
+];
+
+$baseUrl = 'https://raw.githubusercontent.com/MINTILER-DEV/webcomposer/main/src/';
+
+foreach ($files as $file) {
+    $content = file_get_contents($baseUrl . $file);
+    file_put_contents("src/$file", $content);
+}
+
+echo "WebComposer installed successfully!\n";
+echo "Include 'vendor/autoload.php' in your project to use installed packages.\n";
